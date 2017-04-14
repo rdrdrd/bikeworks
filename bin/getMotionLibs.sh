aa1c74a764e04477c31205c54a338cc094b1d795
A B <chickenphat@gmail.com>
2017-04-14 14:27:26 -0700

    Added getMotionLibs.sh and installEmacs.sh

---
+++ bin/getMotionLibs.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo apt-get install -y libjpeg62 libjpeg62-dev libavformat53 libavformat-dev libavcodec53 libavcodec-dev libavutil51 libavutil-dev libc6-dev zlib1g-dev libmysqlclient-dev libpq5 libpq-dev
